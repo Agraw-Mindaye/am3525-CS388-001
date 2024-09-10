95985ccba23c944ce12399e28b6cf9d7d5cd0804
Agraw Mindaye <agrawmindaye@gmail.com>
2024-09-10 15:00:37 -0400

    added Unit1 files

---
+++ TapCounter/app/src/main/java/com/example/tapcounter/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.tapcounter
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    var i = 0;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.b1)
+        val textView = findViewById<TextView>(R.id.textView)
+
+        button.setOnClickListener{
+            i++
+            textView.text = i.toString()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.b1)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
