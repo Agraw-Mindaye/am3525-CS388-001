d9f600224b27eb252b252b8f56d8c3d50b6315a2
Agraw Mindaye <agraw@macbookpro.resource.campus.njit.edu>
2024-11-19 11:27:16 -0500

    fixed duplicate file issues

--- Project6bitfit/app/src/main/java/com/example/project6bitfit/EntryFragment.kt
+++ Project6bitfit/app/src/main/java/com/example/project6bitfit/EntryFragment.kt
@@ -5,11 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.project6bitfit.databinding.FragmentEntryBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
